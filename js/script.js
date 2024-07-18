6d2bf686e54a06c02a7685d46198eb1e76ddc172
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-18 19:12:10 +0200

    [UPD] added a items array and a for cycle

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
-
+            todolist: [
+                {
+                    text: 'scrivere una poesia',
+                    done: '',
+                }, {
+                    text: 'accomodarsi sull\'amaca',
+                    done: '',
+                }, {
+                    text: 'leggere un libro',
+                    done: '',
+                }, {
+                    text: 'rinfrescarmi con 7 calici di vino',
+                    done: '',
+                }
+            ]
         }
     },
     methods: {
